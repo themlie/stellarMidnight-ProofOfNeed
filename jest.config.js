2f7689e4052a82fe2431a5d5033f21688d8c148e
themlie <sena111205@hotmail.com>
2026-09-24 15:58:33 +0300

    build: switch to ESM and add midnight-js / wallet-sdk dependencies
    Jest now runs through ts-jest's ESM preset; adds tsx and a deploy script
    entry for Preprod deployment.

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,16 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
-module.exports = {
+export default {
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
   transform: {
-    ...tsJestTransformCfg,
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 };
